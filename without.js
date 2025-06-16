45412a81ed8b1393ca249d819a61799c8c932c1b
Denise Bagnell <dbagnell@gmail.com>
2025-06-16 13:06:50 -0400

    Added without.js with some basic test cases

---
+++ without.js
@@ -0,0 +1,32 @@
+const without = function(source, itemsToRemove) {
+  let result = [];
+  for (let i = 0; i < source.length; i++) {
+    let item = source[i];
+    if (!itemsToRemove.includes(item)) {
+      result.push(item);
+
+    }
+  }
+return result;
+}
+
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) return false;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) return false;
+  }
+  return true;
+};
+
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
