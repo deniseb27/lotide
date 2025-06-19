e605d6f8ad19b1c82b8602cc153ec87973f8c94b
Denise Bagnell <dbagnell@gmail.com>
2025-06-19 16:33:53 -0400

    created letterPositions function and included basic test cases

---
+++ letterPositions.js
@@ -0,0 +1,36 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) return false;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) return false;
+  }
+  return true;
+};
+
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+
+for (let i = 0; i < sentence.length; i++) {
+  const letter = sentence[i];
+  if (letter === ' ') {
+    continue;
+  }
+    if (results[letter]) {
+      results[letter].push(i);
+    } else {
+      results[letter] = [i];
+    }
+  }
+  return results;
+}
+
+const results = letterPositions("hello");
+console.log(letterPositions("hello"));
