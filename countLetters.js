74fb91803fc15bd0f5afc177a6a7eac2da4e6eec
Denise Bagnell <dbagnell@gmail.com>
2025-06-19 16:48:00 -0400

    Updated two functions and test cases

--- countLetters.js
+++ countLetters.js
@@ -26,9 +26,3 @@ return results;
 
 
 const result1 = letterPositions('hello');
-assertArraysEqual(result1['h'], [0]);
-assertArraysEqual(result1['e'], [1]);
-assertArraysEqual(result1['l'], [2, 3]);
-assertArraysEqual(result1['o'], [4]);
-
-console.log(letterPositions('lighthouse in the house'));
