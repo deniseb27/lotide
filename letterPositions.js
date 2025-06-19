8a8aac185638c81a44da596143e6398dc65663c5
Denise Bagnell <dbagnell@gmail.com>
2025-06-19 16:53:04 -0400

    Updated function's test cases

--- letterPositions.js
+++ letterPositions.js
@@ -32,11 +32,9 @@ for (let i = 0; i < sentence.length; i++) {
   return results;
 }
 
+const result1 = letterPositions("hello");
+
 assertArraysEqual(result1['h'], [0]);
 assertArraysEqual(result1['e'], [1]);
 assertArraysEqual(result1['l'], [2, 3]);
 assertArraysEqual(result1['o'], [4]);
-
-console.log(letterPositions('lighthouse in the house'));
-const results = letterPositions("hello");
-console.log(letterPositions("hello"));
