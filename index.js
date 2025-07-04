8f81b9f3a82923a138f825101a80ab55627d2074
Denise Bagnell <dbagnell@gmail.com>
2025-07-04 15:39:15 -0400

    Updated naming convention, added index.js for export

---
+++ index.js
@@ -0,0 +1,51 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const diceRoller = require('./diceRoller');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const printedNestedArrays = require('./printedNestedArrays');
+const sumRecursive = require('./sumRecursive');
+const sum = require('./sum');
+const sumArgs = require('./sumArgs');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+
+
+
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle,
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  diceRoller: diceRoller,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  flatten: flatten,
+  letterPositions: letterPositions,
+  map: map,
+  printedNestedArrays: printedNestedArrays,
+  sumRecursive: sumRecursive,
+  sum: sum,
+  sumArgs: sumArgs,
+  takeUntil: takeUntil,
+  without: without
+
+};
